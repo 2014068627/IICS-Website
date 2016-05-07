f521a1561bfa8e5bb00383d18af53a85d8451f2e
2014068886 <2014068886@ust-ics.mygbiz.com>
2016-05-07 22:51:59 +0800

    Version 2.0
    This is the Version 2.0 of the IICS Website

---
+++ FinalExamIICSWeb/Version 2.0/theinstitute.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $("#about").click(function(){
+    
+        animateDiv("content .whereabouts");
+        $("#content .whereabouts").html("You have Click on line1");
+    
+    });
+    $("#link2").click(function(){
+    animateDiv("content .mission");
+        $("#content .mission").html("You have Click on line2");
+    
+    });
+    $("#link3").click(function(){
+    animateDiv("right_div .container");
+        $("#right_div .container").html("You have Click on line3");
+    
+    });
+    $("#link4").click(function(){
+    animateDiv("right_div .container");
+        $("#right_div .container").html("You have Click on line4");
+    
+    });
+    
+    function animateDiv(id){
+         //perform your animation here
+    }
+});
+
+
+(function($) {
+	var methods = {
+		init : function() {
+		}
+	};
+	$.fn.JAVAFile = function() {
+		// Method calling logic
+    	if ( methods[method] ) {
+     		return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+   		} else if ( typeof method === 'object' || ! method ) {
+      		return methods.init.apply( this, arguments );
+    	} else {
+     		$$.error( 'Method ' +  method + ' does not exist on this plugin.' );
+    	}  
+	
+		// Put your plugin code here.
+	};
+})(jQuery);
+
