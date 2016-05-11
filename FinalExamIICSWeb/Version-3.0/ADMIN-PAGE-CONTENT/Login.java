3835e61b958c248b63a64bed6c734775ae1b0013
2014068886 <2014068886@ust-ics.mygbiz.com>
2016-05-11 22:58:27 +0800

    Add files via upload

---
+++ FinalExamIICSWeb/Version-3.0/ADMIN-PAGE-CONTENT/Login.java
@@ -0,0 +1,64 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		RequestDispatcher rd;
+		
+		String user = request.getParameter("user");
+		String password = request.getParameter("password");
+		
+		if(user.equals("jm")&&password.equals("1234"))
+		{
+			rd = request.getRequestDispatcher("homepage.html");
+		    rd.forward(request, response);
+		}
+		else if(user.equals("vince")&&password.equals("1234"))
+		{
+			rd = request.getRequestDispatcher("homepage.html");
+			rd.forward(request, response);
+		}
+		else if(user.equals("admin")&&password.equals("1234"))
+		{
+			rd = request.getRequestDispatcher("homepage.html");
+			rd.forward(request, response);
+		}
+		else
+		{
+			rd = request.getRequestDispatcher("adminPage.html");
+			out.println("<font color = red> Username and Password is Invalid!! </font> ");
+			rd.include(request, response);
+		}
+	}
+
+}
